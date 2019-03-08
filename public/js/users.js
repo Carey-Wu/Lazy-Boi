cde4f9c14074e3eb8280a5ab3fed55da694737ef
Toad Sage <kylenewins@gmail.com>
2019-03-07 19:59:41 -0500

    signup validation

--- public/js/users.js
+++ public/js/users.js
@@ -2,15 +2,24 @@ $(document).ready(function(){
 
     $("#signUp").on("click", function(event){
         event.preventDefault()
-        var newUser = {
-            username: $("#newUser").val().trim(),
-            password: $("#newPass").val().trim(),
-            email: $("#newEmail").val().trim()
+        if($("#newUser").val().trim() === "" ||
+        $("#newPass").val().trim() === "" ||
+        $("#newEmail").val().trim() === ""
+        ){
+            console.log("Empty Fields")
+            alert("Enter valid credentials")
+        } else {
+            var newUser = {
+                username: $("#newUser").val().trim(),
+                password: $("#newPass").val().trim(),
+                email: $("#newEmail").val().trim()
+            }
+
+            $.post("/api/users", newUser)
+
+            console.log("User Created" + JSON.stringify(newUser))
+            window.location.href = "/home";
         }
-
-        $.post("/api/users", newUser)
-
-        console.log("User Created" + JSON.stringify(newUser))
     })
 
   
