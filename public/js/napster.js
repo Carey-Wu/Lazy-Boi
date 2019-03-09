ba8ef44404943ac172a1e5e9dc7925a362a77a23
Toad Sage <kylenewins@gmail.com>
2019-03-09 11:02:06 -0500

    track results skeleton

--- public/js/napster.js
+++ public/js/napster.js
@@ -6,6 +6,9 @@ $(document).ready(function () {
     var type = ""
 
     $("#mu-music-results").hide()
+    $("#trackResults").hide()
+    $("#albumResults").hide()
+    $("#artistResults").hide()
 
     console.log("connected")
     $("#searchTrack").on("click", function (event) {
@@ -31,10 +34,41 @@ $(document).ready(function () {
 
             for(var i=0; i < trackResults.length; i ++){
                 console.log(trackResults[i])
-          
+
+                var infoDiv = $("<div></div>")
+                infoDiv.attr("class", "card col-md-12")
+
+                var cardBody = $("<div></div>")
+                cardBody.attr("class", "card-body")
+
+                var title = $("<h3></h3>")
+                title.attr("class", "card-title")
+
+                var artist = $("<h4></h4>")
+                var album = $("<h4></h4>")
+
+                title.text(trackResults[i].name)
+                artist.text("Artist: " + trackResults[i].artistName)
+                album.text("Album: " + trackResults[i].albumName)
+
+                var playButton = $("<button>Play Song!</button>")
+                var addButton = $("<button>Add to Playlist!</button>")
+                playButton.attr("class", "btn btn-success")
+                addButton.attr("class", "btn btn-success")
+
+                title.appendTo(cardBody)
+                artist.appendTo(cardBody)
+                album.appendTo(cardBody)
+
+                playButton.appendTo(cardBody)
+                addButton.appendTo(cardBody)
+
+                cardBody.appendTo(infoDiv)
+                infoDiv.appendTo($("#trackResults"))
             }
         })
         $("#mu-music-results").show()
+        $("#trackResults").show()
 
     })
     $("#searchArtist").on("click", function (event) {
