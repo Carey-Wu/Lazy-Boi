7110fe8019ea9ba3cfea78785dc46584f9b1b525
Toad Sage <kylenewins@gmail.com>
2019-03-12 21:40:23 -0400

    signup/signin validation

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/reauthorize', function (clientRequest, clientResponse) {
   });
 });
 
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
