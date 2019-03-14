bf521aeae93a7fdf09f64966049443c30b55bb49
Carey Wu <cwu@emilcott.com>
2019-03-14 17:50:08 -0400

    presentation version

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ require("./routes/api-routes.js")(app);
 //   });
 // });
 
-db.sequelize.sync({ force: true }).then(function () {
+db.sequelize.sync({ force: false }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
